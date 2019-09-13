f4500806f8deeec63e9c64721d1440d10bff81b6
Sion.Artingstall <Sion.Artingstall@ibm.com>
2019-09-13 12:01:08 +0200

    added api route

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,75 +1,87 @@
 /**
  * Web application
  */
-const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/07-feedback';
+const apiUrl =
+  "https://d526c02f.eu-gb.apiconnect.appdomain.cloud/24-sion-feedback";
 const feedback = {
   // retrieve the existing feedback entries
   get() {
     return $.ajax({
-      type: 'GET',
+      type: "GET",
       url: `${apiUrl}/entries`,
-      dataType: 'json'
+      dataType: "json"
     });
   },
   // add a single guestbood entry
   add(name, email, comment) {
-    console.log('Sending', name, email, comment)
+    console.log("Sending", name, email, comment);
     return $.ajax({
-      type: 'PUT',
+      type: "PUT",
       url: `${apiUrl}/entries`,
-      contentType: 'application/json; charset=utf-8',
+      contentType: "application/json; charset=utf-8",
       data: JSON.stringify({
         name,
         email,
-        comment,
+        comment
       }),
-      dataType: 'json',
+      dataType: "json"
     });
   }
 };
 
 (function() {
-
   let entriesTemplate;
 
   function prepareTemplates() {
-    entriesTemplate = Handlebars.compile($('#entries-template').html());
+    entriesTemplate = Handlebars.compile($("#entries-template").html());
   }
 
   // retrieve entries and update the UI
   function loadEntries() {
-    console.log('Loading entries...');
-    $('#entries').html('Loading entries...');
-    feedback.get().done(function(result) {
-      if (!result.entries) {
-        return;
-      }
+    console.log("Loading entries...");
+    $("#entries").html("Loading entries...");
+    feedback
+      .get()
+      .done(function(result) {
+        if (!result.entries) {
+          return;
+        }
 
-      const context = {
-        entries: result.entries
-      }
-      $('#entries').html(entriesTemplate(context));
-    }).error(function(error) {
-      $('#entries').html('No entries');
-      console.log(error);
-    });
+        const context = {
+          entries: result.entries
+        };
+        $("#entries").html(entriesTemplate(context));
+      })
+      .error(function(error) {
+        $("#entries").html("No entries");
+        console.log(error);
+      });
   }
 
   // intercept the click on the submit button, add the feedback entry and
   // reload entries on success
-  $(document).on('submit', '#addEntry', function(e) {
+  $(document).on("submit", "#addEntry", function(e) {
     e.preventDefault();
 
-    feedback.add(
-      $('#name').val().trim(),
-      $('#email').val().trim(),
-      $('#comment').val().trim()
-    ).done(function(result) {
-      // reload entries
-      loadEntries();
-    }).error(function(error) {
-      console.log(error);
-    });
+    feedback
+      .add(
+        $("#name")
+          .val()
+          .trim(),
+        $("#email")
+          .val()
+          .trim(),
+        $("#comment")
+          .val()
+          .trim()
+      )
+      .done(function(result) {
+        // reload entries
+        loadEntries();
+      })
+      .error(function(error) {
+        console.log(error);
+      });
   });
 
   $(document).ready(function() {
